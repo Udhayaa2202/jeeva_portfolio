8d3ec08bf7c7dbee8e487466b3aa15e337b8f560
bharani2624 <bharani2624baru@gmail.com>
2024-09-22 22:44:11 +0530

    Added Fixed Posiotion For Nav Bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.svg'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
