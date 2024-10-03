f199d071f2a741f6e41a714e819eb5601b19068b
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-10-03 20:01:31 +0530

    Home page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src= {jeeva} alt="" />
-      <h1><span>I'm Jeevitha Kannan,</span> linux developer</h1>
+      <h1><span>I'm Jeevitha Kannan,</span> Linux Developer</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos error qui nobis! Eum voluptate temporibus eveniet molestiae, tenetur quas laborum quaerat assumenda nulla odio suscipit pariatur quisquam modi voluptatum. Doloremque.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
