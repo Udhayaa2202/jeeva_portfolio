654a0e5c8d7bb7d896fd80be90c5be59c4795f0c
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-10-15 22:34:45 +0530

    Altered navbar contents

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close" />
         <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu == "home" ? <img src={underline} alt='' /> : <></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu == "about" ? <img src={underline} alt='' /> : <></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>Services</p></AnchorLink>{menu == "services" ? <img src={underline} alt='' /> : <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={() => setMenu("services")}>My Skills</p></AnchorLink>{menu == "services" ? <img src={underline} alt='' /> : <></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={() => setMenu("work")}>Projects</p></AnchorLink>{menu == "work" ? <img src={underline} alt='' /> : <></>}</li>
         <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu == "contact" ? <img src={underline} alt='' /> : <></>}</li>
       </ul>
