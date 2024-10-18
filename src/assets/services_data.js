77624ef5b9934b18506388ee4ef883d7a2cb74fe
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-10-18 14:10:41 +0530

    Skills 2 added

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,31 +2,31 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"App Developement",
-        s_desc:"A Professional app developer with a creative UI Designing knowledge. Complete backend developer using Java & Kotlin"
+        s_desc:"A Professional app developer with a creative UI Designing knowledge. Complete backend developer using Java & Kotlin."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:""
+        s_name:"Linux Customization Enthusiast",
+        s_desc:"Integrated custom tools and desktop environments to enhance user experience in my customization."
     },
     {
         s_no:"03",
-        s_name:"Social media",
+        s_name:"",
         s_desc:""
     },
     {
         s_no:"04",
-        s_name:"App design",
+        s_name:"---",
         s_desc:""
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
+        s_name:"--",
         s_desc:""
     },
     {
         s_no:"06",
-        s_name:"Content writing",
+        s_name:"------",
         s_desc:""
     },
 ]
