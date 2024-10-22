c8539adcb31a8303de0bc2c05b7ab4759dfd1765
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-10-22 14:33:40 +0530

    Added project src

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,9 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/project1.png'
+import project2_img from '../assets/project_3.svg'
+import project3_img from '../assets/projects2.png'
+// import project4_img from '../assets/project_4.svg'
+// import project5_img from '../assets/project_5.svg'
+// import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
     {
@@ -20,24 +20,25 @@ const mywork_data = [
     },
     {
         w_no:3,
-        w_name:"Web design",
+        w_name:"HyperLand",
+        link:'https://github.com/jeevithakannan2/my-hyprland',
         w_img:project3_img
     },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
+    // {
+    //     w_no:4,
+    //     w_name:"Web design",
+    //     w_img:project4_img
+    // },
+    // {
+    //     w_no:5,
+    //     w_name:"Web design",
+    //     w_img:project5_img
+    // },
+    // {
+    //     w_no:6,
+    //     w_name:"Web design",
+    //     w_img:project6_img
+    // },
 ]
  
 export default mywork_data;
